32c0c6e845d2c6d59ee27679831bf931ea4349f4
abyanfalah <abyanf410@gmail.com>
2022-04-05 10:00:34 +0700

    add modal delete

---
+++ modals/delete.php
@@ -0,0 +1,12 @@
+<div class="modal fade" id="modalDelete" data-backdrop="static">
+	<div class="modal-dialog modal-dialog-centered">
+		<div class="modal-content">
+			<div class="modal-body p-5">
+				<h3>Delete <span class="userNameSpan"></span> from the list?</h3>
+
+				<button class="btn btn-danger" id="btnProceedDelete">Yes</button>
+				<button class="btn btn-primary" data-dismiss="modal">Cancel</button>
+			</div>
+		</div>
+	</div>
+</div>
