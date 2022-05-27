c23aa156af723156567624962e50770406f1918b
abyanfalah <65433269+abyanfalah@users.noreply.github.com>
2022-05-27 13:03:28 +0700

    add truncate modal

---
+++ modals/truncate.php
@@ -0,0 +1,12 @@
+<div class="modal fade" id="modalTruncate" data-backdrop="static">
+	<div class="modal-dialog modal-dialog-centered">
+		<div class="modal-content">
+			<div class="modal-body p-5">
+				<h3>Truncate all data?</h3>
+
+				<button class="btn btn-danger" id="btnProceedTruncate">Yes</button>
+				<button class="btn btn-primary" data-dismiss="modal">Cancel</button>
+			</div>
+		</div>
+	</div>
+</div>
